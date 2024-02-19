fc672bda980092bdfca8d5404da5f763bb8a9158
zaouibahaaeddine <zaouibahaddin5@gmail.com>
2024-02-19 10:12:38 +0100

    Beatified the class "PersonTest"

--- src/test/java/PersonTest.java
+++ src/test/java/PersonTest.java
@@ -1,14 +1,23 @@
+import jdk.jfr.Category;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.zaouibahaddin.Person;
 
 public class PersonTest {
 
+    Person p1 = new Person("Bahaa Eddine" , "ZAOUI", 22);
     @Test
     public void returnsTheRightFullName() {
-        Person p1 = new Person("Bahaa Eddine" , "ZAOUI", 22);
-        System.out.println( (p1.getFullName()));
+
+        // System.out.println( (p1.getFullName()));
         Assertions.assertEquals("Bahaa Eddine ZAOUI", p1.getFullName()); // Will pass
         Assertions.assertEquals("Abdedaka Weld Elhamri", p1.getFullName()); // will fail
     }
+    @Test
+    public void returnsTheRightAgeCategory ()
+    {
+        Assertions.assertEquals(true, p1.isAdult()); // Will pass
+    }
+
+
 }

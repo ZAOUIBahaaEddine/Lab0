e7d6562844ffe44dcdf12e2319faacab1159c513
ZAOUI Bahaa Eddine <zaouibahaddin5@gmail.com>
2024-03-18 00:43:08 +0100

    Added the three files "Prime", "Factorial" and "Fibonacci"
    Added the three files "Prime", "Factorial" and "Fibonacci" manually due to the pushing issue

---
+++ src/main/java/org/zaouibahaddin/Factorial.java
@@ -0,0 +1,19 @@
+package org.zaouibahaddin;
+
+public class Factorial {
+    public static int factorial(int n) {
+        if (n < 0) {
+            throw new IllegalArgumentException("n must be positive");
+        }
+        int result = 1;
+        for (int i = 2; i <= n; i++) {
+            result *= i;
+        }
+        return result;
+
+    }
+
+
+
+}
+

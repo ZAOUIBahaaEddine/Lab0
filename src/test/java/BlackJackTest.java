0aef7541939d60471aaa3e5d359ea44ed6b6f2c7
ZAOUI Bahaa Eddine <zaouibahaddin5@gmail.com>
2024-03-23 10:32:43 -0700

    Updated the BlackJackTest Class with newer tests (To be validated later)

--- src/test/java/BlackJackTest.java
+++ src/test/java/BlackJackTest.java
@@ -16,9 +16,38 @@ public class BlackJackTest {
     }
 
     @Test
-    public void bothPlayersGoTooHigh() {
+    public void testBothPlayersGoTooHigh() {
         int result = blackjack.play(30, 30);
         Assertions.assertEquals(0, result);
+    }
+
+    @Test
+    public void testPlayWithOneValueOver21() {
+        
+        Assertions.assertEquals(18, blackjack.play(18, 22));
+        Assertions.assertEquals(17, blackjack.play(22, 17));
+    }
+
+    @Test
+    public void testPlayWithEqualValues() {
+        // Not sure whether this test is necessary or not, I will check later
+
+        Assertions.assertEquals(20, blackjack.play(20, 20));
+        Assertions.assertEquals(15, blackjack.play(15, 15));
+    }
+
+    @Test
+    public void testPlayWithOneReaches21() {
+        
+        Assertions.assertEquals(21, blackjack.play(20, 21));
+        Assertions.assertEquals(21, blackjack.play(21, 20));
+    }
+
+    @Test
+    public void testBothPlayersGoUnder21() {
+        
+        Assertions.assertEquals(16, blackjack.play(16, 15));
+        Assertions.assertEquals(17, blackjack.play(17, 16));
     }
 
 
